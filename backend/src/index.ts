0bb0772714d2ea9cfd5cf11b910340219f8a3707
leonarjin <izumi.jin@live.com>
2024-06-07 05:48:09 +0100

    vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
-import "reflect-metadata";
-import * as express from "express";
-import { AppDataSource } from "./data-source";
-import * as bodyParser from "body-parser";
-import routes from "./routes";
+import 'reflect-metadata';
+import express from 'express';
+import { AppDataSource } from './data-source';
+import bodyParser from 'body-parser';
+import cors from "cors";
+import routes from './routes';
 
 AppDataSource.initialize().then(async () => {
     const app = express();
 
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(routes);
-    app.listen(3333);
-});
+    app.listen(process.env.PORT || 5432);
+})
