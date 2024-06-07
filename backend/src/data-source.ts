6c3718c79e6b1e9bf1afdb2b5c43d7057cd689aa
leonarjin <izumi.jin@live.com>
2024-06-07 05:59:43 +0100

    fix

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -3,6 +3,18 @@ import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 import { Tasks } from "./entity/Tasks"
 import { Problems } from "./entity/Problems"
+import "dotenv/config"
+
+const getDBConfig = (json: any) => {
+    const newObj = {};
+
+    for (const prop in json){
+        if (!prop.startsWith("DB_")){
+            continue;
+        }
+        const newProp = prop.substring(prop.indexOf("_"))
+    }
+}
 
 export const AppDataSource = new DataSource({
     type: "postgres",
